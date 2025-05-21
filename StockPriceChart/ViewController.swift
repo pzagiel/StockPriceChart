8c0b9738dc879f587d7d00921bec5161063bf2c2
Patrick Zagiel <patrick.zagiel@gmail.com>
2025-05-21 16:46:59 +0200

    Initial Commit

---
+++ StockPriceChart/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StockPriceChart
+//
+//  Created by patrick zagiel on 21/05/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
