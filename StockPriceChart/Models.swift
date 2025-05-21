945defe358a0ad58e6d98bc94e50c36c687fbf6a
Patrick Zagiel <patrick.zagiel@gmail.com>
2025-05-21 18:47:29 +0200

    Change Model for Yahoo prices

--- StockPriceChart/Models.swift
+++ StockPriceChart/Models.swift
@@ -1,19 +1,33 @@
 // Models.swift
 import Foundation
 
-struct StockPrice: Codable {
-    let dateTime: String
+
+struct StockPrice {
+    let date: Date
     let value: Double
-
-    var date: Date? {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        return formatter.date(from: dateTime)
-    }
 }
 
-struct StockData: Codable {
-    let price: [StockPrice]
-    let ticker: String
+
+
+struct YahooFinanceResponse: Codable {
+    let chart: Chart
 }
+
+struct Chart: Codable {
+    let result: [ChartResult]
+}
+
+struct ChartResult: Codable {
+    let timestamp: [Int]
+    let indicators: Indicators
+}
+
+struct Indicators: Codable {
+    let quote: [Quote]
+}
+
+struct Quote: Codable {
+    let close: [Double?]
+}
+
 
